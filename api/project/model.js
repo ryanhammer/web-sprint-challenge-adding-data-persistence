62010bc8524af12f603b6de0a63127b3fb1cb015
rhammer35 <rhammer35@icloud.com>
2021-05-14 10:25:43 -0400

    created required projects endpoints

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,31 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+const getAll = async () => {
+  const projects = await db('projects');
+  projects.forEach(project => {
+    if (project.project_completed === 1) {
+      project.project_completed = true;
+    } else {
+      project.project_completed = false;
+    }
+  })
+  return projects;
+}
+
+const getById = async (project_id) => {
+  const project = await db('project').where({ project_id }).first();
+  if (project.project_completed === 1) {
+    project.project_completed = true;
+  } else {
+    project.project_completed = false;
+  }
+  return project;
+}
+
+const create = async (project) => {
+
+  const [id] = await db('projects').insert(project);
+  return getById(id);
+}
+
+module.exports = { getAll, getById, create }
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,40 @@
-// build your `/api/projects` router here
+const express = require('express');
+
+const Projects = require('./model');
+
+// const { checkCarPayload, checkVinNumberValid, checkCarId, checkVinNumberUnique } = require('./cars-middleware');
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  Projects.getAll()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(next)
+});
+
+router.post(
+  '/',
+  // checkCarPayload,
+  // checkVinNumberValid,
+  // checkVinNumberUnique,
+  (req, res, next) => 
+  {
+    Projects.create(req.body)
+      .then(project => {
+        res.status(201).json(project);
+      })
+      .catch(next);
+  }
+);
+
+router.use( (err, req, res ) => {
+  res.status(err.status || 500).json({
+    note: "Something went wrong in the projects router",
+    message: err.message,
+    stack: err.stack
+  });
+});
+
+module.exports = router;
