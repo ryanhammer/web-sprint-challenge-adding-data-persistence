56fadcbb138e1f61e4ae8b75dd7ed35d8b97b65d
rhammer35 <rhammer35@icloud.com>
2021-05-14 11:28:38 -0400

    created required tasks endpoints

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,30 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+const getAll = async () => {
+  const tasks = await db('tasks as t')
+    .join('projects as p', 't.project_id', '=', 'p.project_id')
+    .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description')
+    .orderBy('t.task_id');
+  
+  tasks.forEach( task => {
+    if (task.task_completed === 1) {
+      task.task_completed === true;
+    } else {
+      task.task_completed === false;
+    }
+  });
+  return tasks;
+}
+
+const getById = async (task_id) => {
+  const task = await db('tasks').where({ task_id }).first();
+  return task;
+}
+
+const create = async (task) => {
+
+  const [task_id] = await db('tasks').insert(task);
+  return getById(task_id);
+}
+
+module.exports = { getAll, getById, create }
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,40 @@
-// build your `/api/tasks` router here
+const express = require('express');
+
+const Tasks = require('./model');
+
+// const { checkCarPayload, checkVinNumberValid, checkCarId, checkVinNumberUnique } = require('./cars-middleware');
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  Tasks.getAll()
+    .then(tasks => {
+      res.status(200).json(tasks);
+    })
+    .catch(next)
+});
+
+router.post(
+  '/',
+  // checkCarPayload,
+  // checkVinNumberValid,
+  // checkVinNumberUnique,
+  (req, res, next) => 
+  {
+    Tasks.create(req.body)
+      .then(task => {
+        res.status(201).json(task);
+      })
+      .catch(next);
+  }
+);
+
+router.use( (err, req, res ) => {
+  res.status(err.status || 500).json({
+    note: "Something went wrong in the tasks router",
+    message: err.message,
+    stack: err.stack
+  });
+});
+
+module.exports = router;
