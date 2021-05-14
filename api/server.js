e22c0bcc1fd254ddb814aa226bf090c0fade0e5d
rhammer35 <rhammer35@icloud.com>
2021-05-14 10:00:14 -0400

    updated server.js file

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
-// build your server here and require it from index.js
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
+const tasksRouter = require('./task/router');
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Someone Check On Ryan, He's Drowning Under All This Backend</h2>`);
+});
+
+module.exports = server
